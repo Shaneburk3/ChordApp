e0cb357fe4c319b4f9a691458716dbd2610ce997
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2025-04-04 19:51:25 +0100

    first commit

---
+++ chordapp/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log("Server listening on port:${port}" )
+})
+
+app.get("/", (req, res) => {
+    res.render("index", {header: "index header", title: "Index"});
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {header: "about header", title: "About"});
+})
+app.get("/contact", (req, res) => {
+    res.render("contact", {header: "contact header", title: "Contact"});
+})
+app.get("/login", (req, res) => {
+    res.render("login", {header: "Login header", title: "Login"});
+})
+
+app.get("/profile", (req, res) => {
+    res.render("profile", {header: "Profile header", title: "Profile"});
+})
+
+
+
+
+
+
