b49c440b55cba6ce93fd6781bf8bb5f30f6224e9
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2025-05-01 10:45:33 +0100

    userController clean up

--- chordapp/controllers/userController.js
+++ chordapp/controllers/userController.js
@@ -17,34 +17,27 @@ exports.registerUser = async (req, res) => {
 
     const userExists = await User.findByEmail(register_email);
     if (userExists) {
-        const msg = ["Account already associated with that email."];
-        //console.log(msg);
+        var msg = ["Account already associated with that email."];
         return res.status(400).render('register', { title: "Register", error_message: msg});
     } else if (register_password1 != register_password2) {
-        const msg = ["Passwords are not the same."];
+        var msg = ["Passwords are not the same."];
         return res.status(400).render('register', { title: "Register", error_message: msg});
     }
     // if it passes the validation and passwords match and unused email -- Create user
     try {
-        console.log("hashing password...")
+        console.log("Hashing password.")
         const hashed_password = await Cipher.createHash(register_password1)
         const creation_date = getDate();
 
-        console.log(`Hashed: ${hashed_password}`)
-        console.log("creating user...");
-
+        console.log("Creating user.");
         await User.create(first_name, last_name, register_email, creation_date, hashed_password);
         const foundUser = await User.findByEmail(register_email);
-        const userID = foundUser.user_ID
+        const userID = foundUser.user_ID;
         await Details.create(userID);
-
-        //You cannot pass other arguments with a redirect, only render can do that.
         return res.redirect('/login');
 
     } catch (error) {
-        //console.log(error);
-        const msg = ['Error creating user'];
-        //console.log("Error creating user.");
+        var msg = ['Error creating user'];
         return res.status(400).render('login', { title: "Login", error_message: msg })
     }
 };
@@ -54,22 +47,18 @@ exports.loginUser = async (req, res) => {
     const { login_email, login_password } = req.body;
     const foundUser = await User.findByEmail(login_email);
     const foundDetails = await Details.findById(foundUser.user_ID);
-    
-    //console.log(`Found this guy: ${JSON.stringify(foundUser)}`)
+    const isMatch = await Cipher.compare(login_password, foundUser.password)
+
     if (!foundUser) {
-        const msg = ['No associated account with that email'];
+        var msg = ['No associated account with that email'];
         console.log(`User ${login_email} does not exist`)
         return res.render('login', { title: "Login", error_message: msg })
-    }
-    // Compare password with hashed stored password.
-    const isMatch = await Cipher.compare(login_password, foundUser.password)
-    if (!isMatch) {
+    } else if (!isMatch) {
         console.log("Password incorrect.")
         const msg = ["Error please try again."];
         return res.render('login', { title: "Login", error_message: msg })
     } else {
         console.log("Found user", foundUser)
-        //console.log(foundUser.user_ID)
         try {
             return res.render(`profile`, { user: foundUser, details: foundDetails, title: `${foundUser.first_name}`});     
         } catch (error) {
@@ -81,20 +70,17 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.updateUser = async (req, res) => {
-    const userID = req.params.user_id || req.body.user_id;
-    try {
-        
-        const user = await User.findById(userID);
-        const userInfo = await Details.findById(userID);
 
-        if (!userInfo || !user) {
+    const userID = req.params.user_id || req.body.user_id;
+    try {        
+        const user = await User.findById(userID);
+        const userDetails = await Details.findById(userID);
+        if (!userDetails || !user) {
             return res.status(404).render('404', { title: "404", error_message: ["User not found!"] })
         } 
-        // If okay...
-        return([user, userInfo])
-        //return res.status(200).render(`profile`, { user: foundUser, details: userDetails, title: `${foundUser.first_name}`});
+        return([user, userDetails])
     } catch (error) {
         console.log(error.message);
-        return res.status(500).render("404", { title: "404", error_message: ["Details not found!"] })
+        return res.status(500).render("404", { title: "404", error_message: ["user data not found"] })
     }
 };
--- chordapp/scripts/script.js
+++ chordapp/scripts/script.js
@@ -1,5 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
 
+    document.getElementById('update_user_form').addEventListener('submit', async (e) => {
+
+        e.preventDefault();
+
+        const user_age = document.getElementById('user_age').value;
+        const user_country = document.getElementById('user_country').value;
+        const user_city = document.getElementById('user_city').value;
+        const user_bio = document.getElementById('user_bio').value;
+
+        const updates = { 
+            users_age: user_age,
+            users_country: user_country,
+            users_city: user_city,
+            users_bio: user_bio
+        };
+
+        try {
+            const response = await fetch('/update', {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(updates)
+            });
+            const result = await response.json();
+            if(!response) {
+                console.log("No Update received")
+            } else {
+                console.log("Profile updated.")
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    });
+/*
     document.getElementById('update_btn').addEventListener('submit', async (e) => {
 
         const user_age = document.getElementById('user_age').value;
@@ -7,16 +41,18 @@ document.addEventListener('DOMContentLoaded', function() {
         const user_city = document.getElementById('user_city').value;
         const user_bio = document.getElementById('user_bio').value;
 
+        console.log("Got updates.")
+
         const response = await fetch('/update', {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
-            bosy: JSON.stringify({user_age, user_country, user_city, user_bio})
+            body: JSON.stringify({user_age, user_country, user_city, user_bio})
         });
 
         const result = await response.json();
         console.log("RESULT: ", result);
 
-    });
-
+    }));
+*/
 
 });
